40302c2a736eb3ed6a3c54de85519be18a3e67de
hothanhloi004 <hothanhloi595@gmail.com>
2025-05-17 21:46:02 +0700

    lab7

---
+++ ASC.Model/Models/ServiceRequest.cs
@@ -0,0 +1,27 @@
+﻿using ASC.Model.BaseTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASC.Model.Models
+{
+    public class ServiceRequest : BaseEntity, IAuditTracker
+    {
+        public ServiceRequest() { }
+        public ServiceRequest(string email) 
+        {
+            this.RowKey = Guid.NewGuid().ToString();
+            this.PartitionKey = email;
+        }
+        public string VechicleName { get; set; }
+        public string VechicleType { get; set; }
+        public string Status { get; set; }
+        public string RequestedServiecs { get; set; }
+        public DateTime? RequestedDate { get; set; }
+        public DateTime? CompletedTime { get; set; }
+        public string ServiceEngineer { get; set; }
+
+    }
+}
